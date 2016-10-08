b27a9190a263de816156444a209cefb347676e23
Non0w <arnaud.cojez@etudiant.univ-lille1.fr>
2016-10-08 13:23:46 +0200

    q2 ok

--- src/application/SubdivCurve.cpp
+++ src/application/SubdivCurve.cpp
@@ -29,13 +29,23 @@ void SubdivCurve::point(int i,const p3d::Vector3 &p) {
 
 
 void SubdivCurve::chaikinIter(const vector<Vector3> &p) {
-  /* TODO : one iteration of Chaikin : input = p, output = you must set the vector _result (vector of Vector3)
-   */
   _result.clear();
 
-  for (int i = 0; i < p.size() - 1; i++) {
+  //Q1
+  int n = p.size();
+
+  for (int i = 0; i < n - 1; i++) {
     Vector3 q2i = (3./4.) * p[i] + (1./4.) * p[i + 1];
     Vector3 q2ip1 = (1./4.) * p[i] + (3./4.) * p[i + 1];
+
+    _result.push_back(q2i);
+    _result.push_back(q2ip1);
+  }
+
+  //Q2
+  if(isClosed()) {
+    Vector3 q2i = (3./4.) * p[n - 1] + (1./4.) * p[0];
+    Vector3 q2ip1 = (1./4.) * p[n - 1] + (3./4.) * p[0];
 
     _result.push_back(q2i);
     _result.push_back(q2ip1);
